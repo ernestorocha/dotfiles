8e6be1a5fa2782d9b75bdbd05050168d28672c1a
Ernesto Rocha <ernestorrocha@gmail.com>
2014-09-23 16:09:42 -0300

    Improved script

--- update_and_install.rb
+++ update_and_install.rb
@@ -1,16 +1,28 @@
 #!/usr/bin/env ruby
 
 GIT_UPDATE = "git submodule foreach git pull"
+DEPENDENCIES = []
 
-
-
-def update_submodules
-  IO.popen(GIT_UPDATE) { |io|
+def run(command)
+  IO.popen(command) { |io|
     io.each_char do |c|
       print c
     end
   }
 end
 
+def update_submodules
+  run(GIT_UPDATE)
+end
+
+def install_dependencies
+  #dependency for syntastic - JS
+  DEPENDENCIES.push('npm install -g jshint')
+
+  DEPENDENCIES.each{ |d| run(d) }
+end
+
+
 
 update_submodules
+install_dependencies
