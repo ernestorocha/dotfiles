e12e86bbb311735491a90197349a60048ae5203c
Ernesto Rocha <ernestorrocha@gmail.com>
2014-10-06 11:27:52 -0300

    Added command to show hidden files on finder

--- mac_os_stuff.sh
+++ mac_os_stuff.sh
@@ -2,3 +2,6 @@
 
 #disable special characters on press and hold - needs re-login
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+#show hidden files
+defaults write com.apple.finder AppleShowAllFiles TRUE
