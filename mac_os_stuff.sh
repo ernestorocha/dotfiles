5e4766903312bae93ad602c62328170fc0756b30
Ernesto Rocha <ernestorrocha@gmail.com>
2014-10-09 12:21:23 -0300

    Added lines to configure Dock speed

--- mac_os_stuff.sh
+++ mac_os_stuff.sh
@@ -5,3 +5,19 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 
 #show hidden files
 defaults write com.apple.finder AppleShowAllFiles TRUE
+
+#delay before the Dock appears when your mouse hits the edge of the screen
+defaults write com.apple.Dock autohide-delay -float 0.5
+
+#default delay
+#defaults delete com.apple.Dock autohide-delay
+
+#speed of the animation when the Dock slides onto the screen
+defaults write com.apple.dock autohide-time-modifier -float 0.5
+
+#defaul
+#defaults delete com.apple.Dock autohide-time-modifier
+
+killall Dock
+
+
