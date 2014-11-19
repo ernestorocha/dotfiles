f0cd4730403a04743360725191d8bbf430038c08
Ernesto Rocha <ernestorrocha@gmail.com>
2014-11-19 11:19:09 -0300

    Updated karabier_import.sh

--- karabiner_import.sh
+++ karabiner_import.sh
@@ -2,22 +2,30 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.musicprevnext_to_modifier_button4_5 1
+$cli set remap.optionL2commandL 1
 /bin/echo -n .
-$cli set remap.musicprevnext_to_arrows_and_modifier 1
+$cli set private.fn_a 1
+/bin/echo -n .
+$cli set remap.commandR2optionR 1
 /bin/echo -n .
 $cli set remap.optionrcommandr 1
 /bin/echo -n .
+$cli set remap.shiftDelete2pipe 1
+/bin/echo -n .
+$cli set remap.musicprevnext_to_arrows_and_modifier 1
+/bin/echo -n .
 $cli set remap.simple_vi_mode 1
 /bin/echo -n .
-$cli set remap.controlDelete2forwarddelete 1
+$cli set remap.musicprevnext_to_modifier_button4_5 1
 /bin/echo -n .
 $cli set remap.consumer_to_fkeys_f7 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
 /bin/echo -n .
-$cli set remap.commandR2optionR 1
+$cli set parameter.blockuntilkeyup_timeout 550
 /bin/echo -n .
-$cli set remap.shiftDelete2pipe 1
+$cli set remap.controlDelete2forwarddelete 1
+/bin/echo -n .
+$cli set remap.commandL2optionL 1
 /bin/echo -n .
 /bin/echo
