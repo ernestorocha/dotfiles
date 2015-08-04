d2063d7b038fcc4b23c68c59a48ed5ea6bf8e887
Ernesto Rocha <ernestorrocha@gmail.com>
2015-08-04 17:32:09 -0400

    Fixed vim install script

--- raspberrypi/install_vim.rb
+++ raspberrypi/install_vim.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 #sudo apt-get build-dep vim-gnome
 #sudo apt-get install mercurial
 #hg clone https://vim.googlecode.com/hg/ vim
@@ -8,18 +10,39 @@
 #
 
 
+def run(command)
+  if !command.kind_of?(Array)
+    cmd  = [command]
+  else
+    cmd = command
+  end
+
+  cmd.each { |line|
+    puts line
+    IO.popen(line) { |io|
+      io.each_char do |c|
+        print c
+      end
+    }
+  }
+
+end
+
 COMMANDS = ["sudo apt-get update", "sudo apt-get install vim-gnome mercurial -y"]
 
 def install_vim
 
   COMMANDS.each { |cmd|
     run cmd
-    Dir.chdir Dir.home
-    run "hg clone https://vim.googlecode.com/hg/ vim"
-    Dir.chdir "#{Dir.home}/vim/src"
-    run "./configure --prefix=/usr/local --with-features=huge --enable-rubyinterp"
-    run "make && sudo make install"
   }
+
+  Dir.chdir Dir.home
+  run "hg clone https://vim.googlecode.com/hg/ vim"
+  Dir.chdir "#{Dir.home}/vim/src"
+  run "./configure --prefix=/usr/local --with-features=huge --enable-rubyinterp"
+  run "make && sudo make install"
 
 end
 
+
+install_vim()
